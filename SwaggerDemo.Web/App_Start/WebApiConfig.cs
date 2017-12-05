eec141de6bbbc6d940b9b5ec5409a8120c737239
karimabd84 <abdelkarim.esti@gmail.com>
2017-12-05 10:46:21 +0100

    Set Swagger as Home page

--- SwaggerDemo.Web/App_Start/WebApiConfig.cs
+++ SwaggerDemo.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Swashbuckle.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -13,6 +14,13 @@ namespace SwaggerDemo.Web
 
             // Web-API-Routen
             config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+               name: "swagger_root",
+               routeTemplate: "",
+               defaults: null,
+               constraints: null,
+               handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
